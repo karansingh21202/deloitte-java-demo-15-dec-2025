8cc38ac9765c373ba8777379e86172548c92ee40
vaman-rao <contact.vaman.rao@gmail.com>
2025-12-15 10:10:07 +0530

    java fundas

--- src/com/deloitte/demo/Hello.java
+++ src/com/deloitte/demo/Hello.java
@@ -3,8 +3,17 @@ package com.deloitte.demo;
 public class Hello {
 
 	public static void main(String[] args) {
-
-		System.out.println("Hello");
+		
+		byte b1 = 10; // -128 to 127 
+		byte b2 = 20;
+		byte b3 = (byte) (b1 + b2);
+		System.out.println(b3);
+		
+		int i = 10;
+		char ch = 'a';
+		System.out.println(ch + i);
+				
+		
 	}
 
 }
