4d7ccd5096b11e2b46c8f44969815fde7c0b0413
vaman-rao <contact.vaman.rao@gmail.com>
2025-12-15 16:46:28 +0530

    java miscellenous packages and classes date time

--- src/com/deloitte/demo/day1/miscellenous/DateTimeDemo.java
+++ src/com/deloitte/demo/day1/miscellenous/DateTimeDemo.java
@@ -1,7 +1,24 @@
 package com.deloitte.demo.day1.miscellenous;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class DateTimeDemo {
 
 	// print current time and date
+
+	public static void main(String[] args) {
+
+		LocalDateTime atPresent = LocalDateTime.now();
+
+		System.out.println(atPresent);
+
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM yyyy HH:mm:ss");
+
+		String printStuff = atPresent.format(formatter);
+
+		System.out.println(printStuff);
+
+	}
 
 }
