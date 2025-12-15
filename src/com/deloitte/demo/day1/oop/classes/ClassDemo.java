fc40fd9efc5f47712f1ed8df4471822b4ec23cf8
vaman-rao <contact.vaman.rao@gmail.com>
2025-12-15 14:20:13 +0530

    class and object

--- src/com/deloitte/demo/day1/oop/classes/ClassDemo.java
+++ src/com/deloitte/demo/day1/oop/classes/ClassDemo.java
@@ -7,21 +7,25 @@ public class ClassDemo {
 //		object creation for a class 
 
 ////		int      num = 10;
-//		Employee emp = new Employee();
-//		System.out.println(emp.id + " " + emp.name + " " + emp.salary);
-//		emp.id = 101;
-//		emp.name = "Sonu";
-//		emp.salary = 90000.50;
-//		System.out.println(emp.id + " " + emp.name + " " + emp.salary);
-//		emp.work();
-		
-		Product product = new Product();
-		product.id = 10;
-		product.name = "Laptop";
-		product.price = 90000.50;
-		System.out.println(product.id + " " + product.name + " " + product.price);
-		product.sale();
-		
+		Employee.officeAddress = "Gachibowli, Hyderabad";
+		Employee.officePhone = 9876543210L;
+		Employee emp = new Employee();
+		System.out.println(emp.id + " " + emp.name + " " + emp.salary);
+		emp.id = 101;
+		emp.name = "Sonu";
+		emp.salary = 90000.50;
+		System.out.println(emp.id + " " + emp.name + " " + emp.salary);
+		emp.work();
+
+		System.out.println(Employee.officeAddress + " " + Employee.officePhone);
+
+//		Product product = new Product();
+//		product.id = 10;
+//		product.name = "Laptop";
+//		product.price = 90000.50;
+//		System.out.println(product.id + " " + product.name + " " + product.price);
+//		product.sale();
+
 	}
 
 }
--- src/com/deloitte/demo/day1/oop/classes/Employee.java
+++ src/com/deloitte/demo/day1/oop/classes/Employee.java
@@ -2,19 +2,24 @@ package com.deloitte.demo.day1.oop.classes;
 
 // entity == class 
 public class Employee {
-	
-	// attributes == variables / fields 
+
+	// attributes == variables / fields
+	// instance fields == belong to object
+//	access using - objectName.fieldName
 	int id;
 	String name;
 	double salary;
-	
-	// functionalities == methods 
-	
+
+	// static fields == belong to class
+//	access using - ClassName.fieldName
+	static long officePhone;
+	static String officeAddress;
+
+	// functionalities == methods
+
 	void work() {
 		System.out.println("Employee is working...");
-		
+
 	}
-	
-	
 
 }
