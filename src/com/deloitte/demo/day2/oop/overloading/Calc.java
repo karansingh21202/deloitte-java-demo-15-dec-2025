3776a71bbee8d05d1d76817571e8ad5d496aab86
vaman-rao <contact.vaman.rao@gmail.com>
2025-12-16 12:39:06 +0530

    overloading

--- src/com/deloitte/demo/day2/oop/overloading/Calc.java
+++ src/com/deloitte/demo/day2/oop/overloading/Calc.java
@@ -6,6 +6,18 @@ public class Calc {
 		return i + j;
 	}
 
+	public int addNums(double i, int j) {
+		return (int) (i + j);
+	}
+
+	public int addNums(int i, double j) {
+		return (int) (i + j);
+	}
+
+	public int addNums(double i, double j) {
+		return (int) (i + j);
+	}
+
 	public int addNums(int i, int j, int k) {
 		return i + j + k;
 	}
@@ -13,4 +25,5 @@ public class Calc {
 	public int addNums(int i, int j, int k, int l) {
 		return i + j + k + l;
 	}
+
 }
